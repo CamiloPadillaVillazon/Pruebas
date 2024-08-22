2e1575afcf03c86ffe37d14bd1b79fee436a0676
Camilito <camilo.padilla@ucb.edu.bo>
2024-08-22 09:36:08 -0400

    Generar Fizz para el 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,12 +1,14 @@
 function generarFizzBuzz(num){
-    if(num === 3){
+    if(num === 3 || num === 5){
         return "Fizz"
     }
 
     if(num%3 === 0){
         return "Fizz"
     }
+
     return num+"";
+
 }
 
 export default generarFizzBuzz;
--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -17,6 +17,10 @@ describe("Fizzbazz", () => {
         expect(generarFizzBuzz(30)).toEqual("Fizz");
     });
 
+    it("generar el Fizz para el 5", () => {
+        expect(generarFizzBuzz(5)).toEqual("Fizz");
+    });
+
 
 });
 
